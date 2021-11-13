7bd5190623f99904628531bee47c455c18ff61da
Kevin Sauvage <kevin@subforce.io>
2021-11-13 21:36:24 +0100

    fix server url

--- src/ApiCall/apiUrl.js
+++ src/ApiCall/apiUrl.js
@@ -2,9 +2,7 @@ export const urls = {
   baseUrl:
     window.location.hostname === 'localhost'
       ? 'http://localhost:5000/'
-      : 'https://socialize-server-92n99kzu1-kevinsauvage.vercel.app/',
+      : 'https://socialize-server.vercel.app/',
   login: 'login',
   register: 'users',
 }
-
-console.log(window.location.hostname === 'localhost')
