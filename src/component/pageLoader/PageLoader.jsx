77bddcc48fe178eb4e7cad31fc10790831b929d3
Kevin Sauvage <kevin@subforce.io>
2021-11-15 20:53:45 +0100

    adding lazy load and page loader

---
+++ src/component/pageLoader/PageLoader.jsx
@@ -0,0 +1,14 @@
+import './PageLoader.scss'
+import Loader from '../loader/Loader'
+
+const PageLoader = () => {
+  return (
+    <div className="PageLoader">
+      <div>
+        <Loader />
+      </div>
+    </div>
+  )
+}
+
+export default PageLoader
--- src/layout/app/App.js
+++ src/layout/app/App.js
@@ -1,13 +1,16 @@
 import './App.scss'
+import { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { AuthProvider } from '../../context/AuthContext'
-import Home from '../../page/home/Home'
-import Login from '../../page/login/Login'
-import Register from '../../page/register/Register'
 import { PostProvider } from '../../context/PostContext'
-import ProfilPage from './../../page/profilPage/ProfilPage'
-import Search from '../../page/search/Search'
+import PageLoader from './../../component/pageLoader/PageLoader'
 import PrivateRoute from './../../helpers/PrivateRoute'
+
+const Search = lazy(() => import('../../page/search/Search'))
+const ProfilPage = lazy(() => import('./../../page/profilPage/ProfilPage'))
+const Register = lazy(() => import('../../page/register/Register'))
+const Login = lazy(() => import('../../page/login/Login'))
+const Home = lazy(() => import('../../page/home/Home'))
 
 const App = () => {
   return (
@@ -15,11 +18,13 @@ const App = () => {
       <AuthProvider>
         <PostProvider>
           <Router>
-            <PrivateRoute path="/" exact component={Home} />
-            <PrivateRoute path="/profil" component={ProfilPage} />
-            <PrivateRoute path="/search" component={Search} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
+            <Suspense fallback={<PageLoader />}>
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+              <PrivateRoute path="/" exact component={Home} />
+              <PrivateRoute path="/profil" component={ProfilPage} />
+              <PrivateRoute path="/search" component={Search} />
+            </Suspense>
           </Router>
         </PostProvider>
       </AuthProvider>
